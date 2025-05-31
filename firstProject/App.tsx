b8afbe60a2ad01ecf5da0f351b6c21cff947daaf
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-05-31 12:05:03 +0530

    Add actual contents of firstProject

---
+++ firstProject/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+ 
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View><Text>JSR</Text></View>
+    <View>
+       <Text>Jay Shree Ram</Text>
+       <Text>Jay Shree Ram</Text>
+       <Text>Jay Shree Ram</Text>
+       <Text>Jay Shree Ram</Text>
+       <Text>Jay Shree Ram</Text>
+       <Text>Jay Shree Ram</Text>
+    </View>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
