04834bb70155c069fd903312ea11fa0a74e58c7c
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-06-04 23:54:09 +0530

    Project 2 Styling completed

--- firstProject/App.tsx
+++ firstProject/App.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
 import FancyCard from './components/FancyCard'
+import ActionCard from './components/ActionCard'
+import ContactList from './components/ContactList'
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-      <Text>App</Text>
-      </ScrollView>
       <FlatCards />
       <ElevatedCards/>
       <FancyCard />
+      <ActionCard />
+      <ContactList />
+      </ScrollView>
     </SafeAreaView>
   )
 }
