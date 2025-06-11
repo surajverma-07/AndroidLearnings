f29d096504264528d0f49d37fcd27415cb661cc1
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-06-11 22:32:13 +0530

    project completed

--- CurrencyConvertor/App.tsx
+++ CurrencyConvertor/App.tsx
@@ -1,55 +1,148 @@
-import { View, Text,StyleSheet, ImageSourcePropType, Image, SafeAreaView } from 'react-native'
+import React, {useState} from 'react';
 
-import React,{PropsWithChildren} from 'react'
+import {
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
 
+//Constants
+import {currencyByRupee} from './constants';
+//Component
+import CurrencyButton from './components/CurrencyButton';
 
-type CardProps = PropsWithChildren<{
-  imageUrl: ImageSourcePropType
-}> 
+import Snackbar from 'react-native-snackbar';
 
-const Card = ({ text, imageUrl }: { text: string; imageUrl: ImageSourcePropType }) => {
-   return(
-     <View style={styles.card}>
-        <Text style={[styles.cardText]}>{text}</Text>
-        <Image source={imageUrl} style={{width: 100, height: 100}} />
-     </View>
-   )
-}
 const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
+  const [targetCurrency, setTargetCurrency] = useState('');
+
+  const buttonPressed = (targetValue: Currency) => {
+    if (!inputValue || inputValue.trim() === '') {
+      return Snackbar.show({
+        text: 'Enter a value to convert',
+        backgroundColor: '#EA7773',
+        textColor: '#000000',
+      });
+    }
+
+    const inputAmount = parseFloat(inputValue);
+    if (isNaN(inputAmount) || inputAmount <= 0) {
+      return Snackbar.show({
+        text: 'Enter a correct number greater than 0',
+        backgroundColor: '#EA7773',
+        textColor: '#000000',
+      });
+    }
+
+    const convertedValue = inputAmount * targetValue.value;
+    const result = `${targetValue.symbol} ${convertedValue.toFixed(2)}`;
+    setResultValue(result);
+    setTargetCurrency(targetValue.name);
+  };
   return (
-    <SafeAreaView>
-      <View>
-         <Text>Currency Converter</Text>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.rupeesContainer}>
+            <Text style={styles.rupee}>₹</Text>
+            <TextInput
+              maxLength={14}
+              value={inputValue}
+              onChangeText={setInputValue}
+              keyboardType="number-pad"
+              placeholder="Enter amount in Rupees"
+            />
+          </View>
+          {resultValue && <Text style={styles.resultTxt}>{resultValue}</Text>}
+        </View>
+        <View style={styles.bottomContainer}>
+          <FlatList
+            numColumns={3}
+            data={currencyByRupee}
+            keyExtractor={item => item.name}
+            renderItem={({item})=>(
+               <Pressable
+                style={[
+                  styles.button,
+                  targetCurrency === item.name && styles.selected,
+                ]}
+                onPress={() => buttonPressed(item)}>
+                <CurrencyButton {...item} />
+              </Pressable>
+            )}
+          />
+        </View>
       </View>
-    </SafeAreaView>
-  )
-}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-  card:{
+  container: {
+    flex: 1,
+    backgroundColor: '#515151',
+  },
+  topContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+  resultTxt: {
+    fontSize: 32,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupee: {
+    marginRight: 8,
+
+    fontSize: 22,
+    color: '#000000',
+    fontWeight: '800',
+  },
+  rupeesContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputAmountField: {
+    height: 40,
+    width: 200,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#FFFFFF',
+  },
+  bottomContainer: {
+    flex: 3,
+  },
+  button: {
+    flex: 1,
+
+    margin: 12,
+    height: 60,
+
+    borderRadius: 12,
     backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-    margin: 10,
-    shadowColor: '#000',
+    elevation: 2,
     shadowOffset: {
-      width: 0,
-      height: 2,
+      width: 1,
+      height: 1,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
   },
-  cardText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10,
+  selected: {
+    backgroundColor: '#ffeaa7',
   },
-  cardImage: {
-    width: '100%',
-    height: 150,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-})
-export default App
+});
+
+export default App;
